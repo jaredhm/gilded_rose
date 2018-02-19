5990fc7e322c1f8da2dffa607ce51c64a5337c4b
Jared MacFarlane <jmacfarlane@athenahealth.com>
2018-02-19 12:12:24 -0500

    implementing endpoint to see availability (finally). booking endpoint should be easy enough

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
-const inn = require('./lib/inn'); // Create the initial inn object
+const Inn = require('./lib/inn');
 const bookingLib = require('./lib/booking');
+
+const inn = new Inn(); // Create the initial inn object
 
 /*
  * First Endpoint: checking availabilty
@@ -10,13 +12,24 @@ const bookingLib = require('./lib/booking');
  * accomodate the specified number of guests, pieces of luggage, whether
  * or not the room is clean
  */
-app.get('/guests/:numGuests/luggage/:luggageCount', function (req, res) {
+app.get('/book/guests/:numGuests/luggage/:luggageCount', function (req, res) {
   // Get the available rooms based on constraints
-  bookingLib.availableRooms(
+  const booking = bookingLib.getAccomodations(
     req.params.numGuests,
     req.params.luggageCount,
-    inn
+    inn,
+    false
   );
+  if(booking === undefined) {
+    res.status(404).json({
+      error: 'Could not accomodate booking'
+    });
+  } else {
+    booking.error = '';
+    res.status(200).json({
+      booking: booking
+    });
+  }
 })
 
 app.listen(3000, () => console.log('Booking app listening on port 3000!'));
